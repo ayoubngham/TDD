562d37ad5987fbc6cfa252c6c21274fac4ce355b
ayoubngham <pro.ayoubngham@gmail.com>
2023-10-02 12:56:18 +0200

    code pour question 1 exo1

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,25 @@
 class FizzBuzz:
-    def affiche ():
+    def affiche (self):
 
         for i in range(1, 101):
             output = ""
 
+            if i % 3 == 0 and i % 5 == 0:
+                output ="FrisBee"
+            elif i % 3 == 0:
+                output ="Fizz"
+            elif i % 5 == 0:
+                output ="Buzz"
+            else:
+                output = str(i)
             
+            print(output)
+    affiche()
+    
+
+
+
+
 
 
 
