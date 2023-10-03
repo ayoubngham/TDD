0300b874f9c34b6b78e1208bccc9e9b2ed4cf78c
ayoubngham <pro.ayoubngham@gmail.com>
2023-10-03 12:31:04 +0200

    seconde modification rajout de l'argument n

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 class FizzBuzz:
     def affiche (self, n):
 
-        for i in range(1, 101):
+        for i in range(1, n + 1):
             output = ""
             if i % 3 == 0 and i % 5 == 0:
                 output ="FrisBee"
