273d2c87d237e4c0b00e4d7c56667d4496a49e29
ayoubngham <pro.ayoubngham@gmail.com>
2023-10-03 12:21:14 +0200

    version erronée du au changement if % 15

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,19 +1,20 @@
 class FizzBuzz:
     def affiche ():
-
+        
         for i in range(1, 101):
             output = ""
 
-            if i % 3 == 0 and i % 5 == 0:
-                output ="FrisBee"
-            elif i % 3 == 0:
-                output ="Fizz"
-            elif i % 5 == 0:
-                output ="Buzz"
-            else:
-                output = str(i)
+        if i % 15 == 0:
+            output ="FrisBee"
             
-            print(output)
+        elif i % 3 == 0:
+            output ="Fizz"
+        elif i % 5 == 0:
+            output ="Buzz"
+        else:
+            output = str(i)
+            
+        print(output)
     affiche()
 
 
